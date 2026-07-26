fac3b03bb82bd85e5f36ebf71dc1df908abe88dd
luperttrading-lab <lupert.trading@gmail.com>
2026-07-26 11:42:27 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,8 @@
-/* Wein-Berater Service Worker — wein-berater-v5_75 */
-const CACHE = "wein-berater-v5_75";
+/* Wein-Berater Service Worker — wein-berater-v5_76 */
+const CACHE = "wein-berater-v5_76";
 const ASSETS = [
   "./",
   "./index.html",
-  "./wein-berater-v5_75.html",
   "./manifest.json",
   "aromen/Akazie.png",
   "aromen/Ananas.png",
